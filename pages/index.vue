7e8dec5cf3a145d9baccb3413704d72364c1a910
Leao04 <lordvergil12345@hotmail.com>
2020-10-10 18:25:25 -0300

    .

--- pages/index.vue
+++ pages/index.vue
@@ -6,38 +6,27 @@
       :interval="4000"
       controls
       indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="280"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
     
       <b-carousel-slide
-        caption="First slide"
         text="Somente nos cinemas.."
         img-src="../assets/img/spider.jpg"
+        img-height="200px"
       ></b-carousel-slide>
 
-    
-      <b-carousel-slide img-src="../assets/img/end.jpg">
-      </b-carousel-slide>
+     <b-carousel-slide
+        text="Somente nos cinemas.."
+        img-src="../assets/img/end.jpg"
+      ></b-carousel-slide>
 
-      
-      <b-carousel-slide img-src="../assets/img/inter.jpg"></b-carousel-slide>
-
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="280"
-            src="../assets/img/end.jpg"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
+       <b-carousel-slide
+        text="Somente nos cinemas.."
+        img-src="../assets/img/inter.jpg"
+      ></b-carousel-slide>
+     
 
     </b-carousel>
   </div>
