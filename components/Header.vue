a5682fb0318ac362afe991f37520d65a1c367a96
Leao04 <lordvergil12345@hotmail.com>
2020-10-10 16:47:41 -0300

    j

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,47 @@
 <template>
   <div>
-    <div>
-      <b-navbar type="light" variant="dark">
-        <b-navbar-nav>
-          <NuxtLink to="/">Início</NuxtLink>
-          <div class="menu1">
-            <NuxtLink to="/">Cine Pipocando</NuxtLink>
-          </div>
-          <div class="menu2">
-            <NuxtLink to="/">Lançamentos</NuxtLink>
-          </div>
-          <div class="menu3">
-            <NuxtLink to="/">Ingressos</NuxtLink>
-          </div>
-        </b-navbar-nav>
-      </b-navbar>
-    </div>
+     <div><div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <NuxtLink to="/">Início</NuxtLink>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <div class="menu1">
+        <NuxtLink to="/">Cine Pipoca</NuxtLink>
+        </div>
+        <div class="menu">
+        <NuxtLink to="/">Lançamentos</NuxtLink>
+        </div>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div></div>
   </div>
 </template>
 
