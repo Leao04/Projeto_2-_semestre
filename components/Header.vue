0cd9c2452497020848e9010dfafa74fd72a17443
Leao04 <lordvergil12345@hotmail.com>
2020-10-10 17:31:38 -0300

    l

--- components/Header.vue
+++ components/Header.vue
@@ -2,41 +2,34 @@
   <div>
      <div><div>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <NuxtLink to="/">Início</NuxtLink>
-
+  
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <div class="menu1">
-        <NuxtLink to="/">Cine Pipoca</NuxtLink>
-        </div>
-        <div class="menu">
-        <NuxtLink to="/">Lançamentos</NuxtLink>
-        </div>
+        <b-nav-item href="#">Início</b-nav-item>
+        <b-nav-item href="#">Cine Pipoca</b-nav-item>
+        <b-nav-item href="#">Lançamentos</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Pesquisas"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Pesquisar</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
+        <b-nav-item-dropdown text="Idioma" right>
           <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
+          <b-dropdown-item href="#">PT-BR</b-dropdown-item>
         </b-nav-item-dropdown>
 
         <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
-            <em>User</em>
+            <em>Usuário</em>
           </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          <b-dropdown-item href="#">Entrar</b-dropdown-item>
+          <b-dropdown-item href="#">Cadastrar-se</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
